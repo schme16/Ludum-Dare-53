1df11532f0ad0f8fe3666d922a6e7e315beb1889
Shane Gadsby <schme16@gmail.com>
2023-04-29 16:28:07 +1000

    Updating the unity files with the new stubs

---
+++ Assets/data/scripts/PlayerScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	// Start is called before the first frame update
+	public float speed = 1;
+	public float speedModifier = 0;
+	void Start() {
+	}
+
+	// Update is called once per frame
+	void Update() {
+	}
+}
