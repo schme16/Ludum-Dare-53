ba5f30024ee7aa9dca1d9afe6e98a7d604667211
Shane Gadsby <schme16@gmail.com>
2023-05-01 02:00:01 +1000

    Added the seagull, and a script for it Minor change to the food hold position

--- Assets/data/scripts/PlayerScript.cs
+++ Assets/data/scripts/PlayerScript.cs
@@ -11,7 +11,7 @@ public class PlayerScript : MonoBehaviour {
 	public Transform mealHolder;
 	Rigidbody rb;
 	private Camera cam;
-	
+
 	void Start() {
 		cam = Camera.main;
 		rb = transform.GetComponent<Rigidbody>();
@@ -21,16 +21,20 @@ public class PlayerScript : MonoBehaviour {
 	void Update() {
 		var x = Input.GetAxis("Horizontal");
 		var y = Input.GetAxis("Vertical");
-		
+
 		if (x != 0) {
-			 Quaternion rotation = Quaternion.Euler(0, transform.eulerAngles.y + (x * turnSpeed), 0);
+			Quaternion rotation = Quaternion.Euler(0, transform.eulerAngles.y + (x * turnSpeed), 0);
 			rb.MoveRotation(rotation);
 		}
-		
+
 		if (y != 0) {
 			rb.AddForce(transform.forward * (y * (speed + speedModifier)));
 		}
-		wheels[0].Rotate(new Vector3(0, rb.velocity.x, 0));
-		wheels[1].Rotate(new Vector3(0, rb.velocity.x, 0));
+
+		var velocity = Vector3.Dot(rb.velocity, transform.forward);
+
+		wheels[0].Rotate(new Vector3(0, velocity, 0));
+		wheels[1].Rotate(new Vector3(0, velocity, 0));
+		wheels[2].Rotate(new Vector3(0, 0, velocity));
 	}
 }
